11e6c0fb0887cf45a9967e4e44a0484a6b3bc78a
unknown <muntasirudoy4@gmail.com>
2021-05-02 22:15:28 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,44 +1,4 @@
 
-$('.add-to-cart').on('click', function () {
-        var cart = $('.cart-nav');
-        var imgtodrag = $(this).parent('.item').find("img").eq(0);
-        if (imgtodrag) {
-            var imgclone = imgtodrag.clone()
-                .offset({
-                top: imgtodrag.offset().top,
-                left: imgtodrag.offset().left
-            })
-                .css({
-                'opacity': '0.8',
-                    'position': 'absolute',
-                    'height': '150px',
-                    'width': '150px',
-                    'z-index': '100'
-            })
-                .appendTo($('body'))
-                .animate({
-                'top': cart.offset().top + 10,
-                    'left': cart.offset().left + 10,
-                    'width': 75,
-                    'height': 75
-            }, 1000, 'easeInOutExpo');
-            
-            setTimeout(function () {
-                cart.effect("shake", {
-                    times: 2
-                }, 200);
-            }, 1500);
-
-            imgclone.animate({
-                'width': 0,
-                    'height': 0
-            }, function () {
-                $(this).detach()
-            });
-        }
-    });    
-
-
 
 
     let count = 0;
@@ -57,19 +17,29 @@ $('.add-to-cart').on('click', function () {
           'position': 'absolute',
           'height': '150px',
           'width': '150px',
-          'z-index': '100'
-        }).appendTo($('body')).animate({
-          'top': cart.offset().top + 20,
-          'left': cart.offset().left + 30,
-          'width': 75,
-          'height': 75
-        }, 1000, 'easeInOutExpo');
+          'z-index': '11'
+        })
+        
+        
+        .appendTo($('body')).animate({
+        'top': cart.offset().top + 30,
+    'left': cart.offset().left + 30,
+       'width': 20,
+      'height': 20
+    }, 1000, 'easeInOutExpo');
     
         setTimeout(function(){
           count++;
           $(".cart-nav .item-count").text(count);
         }, 1500);
-    
+
+        setTimeout(function () {
+            cart.effect('shake', {
+            times: 3
+             }, 200);
+            }, 1500);
+
+
         imgclone.animate({
           'width': 0,
           'height': 0
